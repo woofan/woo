6748556e4a97afe04787155b2c1e5c0f2bc9c4db
woofan <495464616@qq.com>
2016-08-10 17:08:49 +0800

    8.10

---
+++ 任务十八/task18.js
@@ -0,0 +1,22 @@
+var input = document.getElementById("getnum");//id不能用input
+var main_div = document.getElementById("d");
+function leftin(){
+	var check = /^\d+$/;
+	if(check.test(getnum.value)){
+		var d = document.createElement("div");
+		d.style.background="red";
+		d.style.color="white";
+		d.style.width="40px";
+		d.style.height="30px";
+		d.style.cssFloat="left";//ie:obj.style.styleFloat;firefox,chrome：obj.style.cssFloat；
+		d.style.marginLeft="10px";
+		d.style.marginTop="10px";
+		d.style.textAlign="center";
+		d.style.paddingTop="10px";
+		d.innerHTML=getnum.value;
+		document.body.appendChild(d);
+	}
+	else{
+		alert("请输入数字");
+	}
+}
